52d4ba43019768d880c7beb414ea9469268af4ff
Sunit <56727564+sunit30@users.noreply.github.com>
2022-12-29 13:07:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -65,3 +65,35 @@ long.addEventListener("click", playSound);
 
 let toggle = document.getElementById("toggle");
 toggle.addEventListener("click", toggleView);
+
+
+// Get modal element
+var modal = document.getElementById('simpleModal');
+// Get open modal button
+var modalBtn = document.getElementById('modalBtn');
+// Get close button
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+// Listen for open click
+modalBtn.addEventListener('click', openModal);
+// Listen for close click
+closeBtn.addEventListener('click', closeModal);
+// Listen for outside click
+window.addEventListener('click', outsideClick);
+
+// Open modal
+function openModal(){
+  modal.style.display = 'block';
+}
+
+// Close modal
+function closeModal(){
+  modal.style.display = 'none';
+}
+
+// Click outside and close
+function outsideClick(e){
+  if(e.target == modal){
+    modal.style.display = 'none';
+  }
+}
